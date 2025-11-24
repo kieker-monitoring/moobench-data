a217159ba9ae7ccd9f050a8b247a0c6657821ec3
Kieker CI <ci-bot@kieker.org>
2025-11-24 14:34:29 +0000

    Benchmark Results 175e234be5a21008c9a7d10d3720ecf36ad33114

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763985623356,
+  "lastUpdate": 1763994869711,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -154795,6 +154795,54 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3419.32,
             "range": "3528.9",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "175e234be5a21008c9a7d10d3720ecf36ad33114",
+          "message": "Add zipkin+cassandra option",
+          "timestamp": "2025-11-24T14:37:55+01:00",
+          "tree_id": "e26326a67146370647e9ceb2550359741b519ad6",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/175e234be5a21008c9a7d10d3720ecf36ad33114"
+        },
+        "date": 1763994869679,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.7349,
+            "range": "44.7353",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 139.219,
+            "range": "140.11",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3356.57,
+            "range": "3461.35",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3140.19,
+            "range": "3250.65",
             "unit": "ns"
           }
         ]
