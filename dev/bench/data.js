268056eebb52dee590dc1f5df3719d54734be990
Kieker CI <ci-bot@kieker.org>
2025-12-03 05:33:44 +0000

    Benchmark Results a0996b23c15a3a0c1487a3409e3e780b19f87e8e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764737217550,
+  "lastUpdate": 1764740024630,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -182749,6 +182749,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 1953.18,
             "range": "1954.17",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "a0996b23c15a3a0c1487a3409e3e780b19f87e8e",
+          "message": "Fix check",
+          "timestamp": "2025-11-24T14:59:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/a0996b23c15a3a0c1487a3409e3e780b19f87e8e"
+        },
+        "date": 1764740024599,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.6605,
+            "range": "44.6605",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 826.479,
+            "range": "827.45",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 4831.12,
+            "range": "4832.15",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5032.1,
+            "range": "5033.1",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 1953.2,
+            "range": "1954.16",
             "unit": "ns"
           }
         ]
