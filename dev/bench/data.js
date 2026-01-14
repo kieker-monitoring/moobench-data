f5b0a3b4d318739fbc732390ec1a9d5c115bd1cb
Kieker CI <ci-bot@kieker.org>
2026-01-14 05:51:44 +0000

    Benchmark Results a0996b23c15a3a0c1487a3409e3e780b19f87e8e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768367053895,
+  "lastUpdate": 1768369904546,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -191189,6 +191189,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 1954.47,
             "range": "1956.45",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "a0996b23c15a3a0c1487a3409e3e780b19f87e8e",
+          "message": "Fix check",
+          "timestamp": "2025-11-24T14:59:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/a0996b23c15a3a0c1487a3409e3e780b19f87e8e"
+        },
+        "date": 1768369904513,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.6732,
+            "range": "44.6733",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 824.736,
+            "range": "826.81",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 4852.46,
+            "range": "4853.1",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5113.61,
+            "range": "5114.08",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 2003.37,
+            "range": "2004.15",
             "unit": "ns"
           }
         ]
