0d45ea877aa9e23f69a894a793dd1b8c21964eea
Kieker CI <ci-bot@kieker.org>
2026-01-24 05:42:10 +0000

    Benchmark Results a0996b23c15a3a0c1487a3409e3e780b19f87e8e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769230598529,
+  "lastUpdate": 1769233330309,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -193181,6 +193181,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 1964.64,
             "range": "1965.65",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "a0996b23c15a3a0c1487a3409e3e780b19f87e8e",
+          "message": "Fix check",
+          "timestamp": "2025-11-24T14:59:30Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/a0996b23c15a3a0c1487a3409e3e780b19f87e8e"
+        },
+        "date": 1769233330275,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.6636,
+            "range": "44.6636",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 818.819,
+            "range": "819.865",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 4837.43,
+            "range": "4838.44",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5094.16,
+            "range": "5097.62",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 2026.49,
+            "range": "2026.8",
             "unit": "ns"
           }
         ]
