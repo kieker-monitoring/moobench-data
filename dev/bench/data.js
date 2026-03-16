d5db58760f949bbb84431d23ea75c66ca4676295
Kieker CI <ci-bot@kieker.org>
2026-03-16 06:10:24 +0000

    Benchmark Results b458bb9f4220e5ddf7c6b45c83e39b6165708b6a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773641355695,
+  "lastUpdate": 1773641424458,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -167913,6 +167913,52 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3441.85,
             "range": "3564.47",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "b458bb9f4220e5ddf7c6b45c83e39b6165708b6a",
+          "message": "Fix URL",
+          "timestamp": "2026-03-06T09:13:35Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/b458bb9f4220e5ddf7c6b45c83e39b6165708b6a"
+        },
+        "date": 1773641424425,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 29.1435,
+            "range": "29.3134",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 154.318,
+            "range": "154.373",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 4022.05,
+            "range": "4089.82",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 4089.99,
+            "range": "4186.73",
             "unit": "ns"
           }
         ]
