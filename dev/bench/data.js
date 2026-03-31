fcd1f134c0f53b3b540c61a259c7c39b0e1c805d
Kieker CI <ci-bot@kieker.org>
2026-03-31 13:48:42 +0000

    Benchmark Results 3eae13c65d171673b5a9b7e9cca8b6c6ad48383e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774941676401,
+  "lastUpdate": 1774964922193,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -43903,6 +43903,54 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 1204.88,
             "range": "1207.79",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "3eae13c65d171673b5a9b7e9cca8b6c6ad48383e",
+          "message": "Update logback to 1.5.32",
+          "timestamp": "2026-03-31T15:03:23+02:00",
+          "tree_id": "55ca5ff440bf3bb8ae6aa2c072427ad4e5682020",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3eae13c65d171673b5a9b7e9cca8b6c6ad48383e"
+        },
+        "date": 1774964922162,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 28.7708,
+            "range": "28.7785",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 39.8747,
+            "range": "39.8761",
+            "unit": "ns"
+          },
+          {
+            "name": "No collection",
+            "value": 440.294,
+            "range": "440.298",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 1223.18,
+            "range": "1223.95",
             "unit": "ns"
           }
         ]
