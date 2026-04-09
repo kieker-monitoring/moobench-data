3aea95a7c50b3754243c0f5a04501a7f57efece1
Kieker CI <ci-bot@kieker.org>
2026-04-09 17:25:21 +0000

    Benchmark Results 02b3578e0a09d82ed5713e1f32d130345301803e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775755020101,
+  "lastUpdate": 1775755521060,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -175401,6 +175401,54 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3098.71,
             "range": "3182.98",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "02b3578e0a09d82ed5713e1f32d130345301803e",
+          "message": "Check whether the given folder is absolute",
+          "timestamp": "2026-04-09T18:28:45+02:00",
+          "tree_id": "66a32af95a331ce28c55ae944a8b0b6d396773a4",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/02b3578e0a09d82ed5713e1f32d130345301803e"
+        },
+        "date": 1775755521024,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.8832,
+            "range": "44.8834",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 146.325,
+            "range": "146.446",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3392.29,
+            "range": "3455.66",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3479.28,
+            "range": "3568.92",
             "unit": "ns"
           }
         ]
