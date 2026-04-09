7c67dfc415a8a3b350e7d743fde59ce6a5741e65
Kieker CI <ci-bot@kieker.org>
2026-04-09 18:13:56 +0000

    Benchmark Results 02b3578e0a09d82ed5713e1f32d130345301803e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775755778247,
+  "lastUpdate": 1775758436710,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -206579,6 +206579,60 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 1805.11,
             "range": "1986.18",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "02b3578e0a09d82ed5713e1f32d130345301803e",
+          "message": "Check whether the given folder is absolute",
+          "timestamp": "2026-04-09T18:28:45+02:00",
+          "tree_id": "66a32af95a331ce28c55ae944a8b0b6d396773a4",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/02b3578e0a09d82ed5713e1f32d130345301803e"
+        },
+        "date": 1775758436671,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.897,
+            "range": "44.8971",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 818.122,
+            "range": "818.449",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 4902.19,
+            "range": "4903.63",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5190.22,
+            "range": "5191.37",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 2004.02,
+            "range": "2004.71",
             "unit": "ns"
           }
         ]
