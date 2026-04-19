4539a38134c8e2ed46c49cb3c9580de6e0d7c1d1
Kieker CI <ci-bot@kieker.org>
2026-04-19 13:52:44 +0000

    Benchmark Results 976abd7627f67ebb5862f651c5a1970710c08e12

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776606097428,
+  "lastUpdate": 1776606764454,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -46407,6 +46407,54 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 1010.74,
             "range": "1011.02",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "976abd7627f67ebb5862f651c5a1970710c08e12",
+          "message": "Fix folder name",
+          "timestamp": "2026-04-19T14:19:27+02:00",
+          "tree_id": "f734c617118d546fbe98878891474f769f0cba58",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/976abd7627f67ebb5862f651c5a1970710c08e12"
+        },
+        "date": 1776606764415,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.9831,
+            "range": "44.9861",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 48.6614,
+            "range": "48.6615",
+            "unit": "ns"
+          },
+          {
+            "name": "No collection",
+            "value": 462.659,
+            "range": "462.694",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 996.608,
+            "range": "997.115",
             "unit": "ns"
           }
         ]
