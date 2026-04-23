4edb7d713e4ca8d4f12499dafc12387ebcf2c319
Kieker CI <ci-bot@kieker.org>
2026-04-23 06:26:06 +0000

    Benchmark Results 82f6fcc8334c9513bbb9d378b3a5e287d2c2e56e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776925244606,
+  "lastUpdate": 1776925566485,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -182751,6 +182751,52 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 4261.23,
             "range": "4349.68",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "82f6fcc8334c9513bbb9d378b3a5e287d2c2e56e",
+          "message": "Fix OpenTelemetry environment variable",
+          "timestamp": "2026-04-21T16:41:42Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/82f6fcc8334c9513bbb9d378b3a5e287d2c2e56e"
+        },
+        "date": 1776925566451,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.0281,
+            "range": "45.0281",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 146.256,
+            "range": "146.494",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3468.36,
+            "range": "3559.65",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3046.1,
+            "range": "3131.55",
             "unit": "ns"
           }
         ]
