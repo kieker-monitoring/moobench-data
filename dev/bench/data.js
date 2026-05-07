c764bdae8ae04b8c9fb8085678afdc826a78fcdd
Kieker CI <ci-bot@kieker.org>
2026-05-07 07:07:14 +0000

    Benchmark Results 607e1be39104f58acdb760a1dd4d7bdea47a01f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778136555656,
+  "lastUpdate": 1778137634636,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -186547,6 +186547,52 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3520.02,
             "range": "3612.74",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "607e1be39104f58acdb760a1dd4d7bdea47a01f6",
+          "message": "Remove MOOBENCH_CONFIGURATIONS so all default configurations are used",
+          "timestamp": "2026-04-30T10:09:37Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/607e1be39104f58acdb760a1dd4d7bdea47a01f6"
+        },
+        "date": 1778137634602,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.1206,
+            "range": "45.1207",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 140.416,
+            "range": "141.636",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3162.67,
+            "range": "3240.67",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3696.75,
+            "range": "3795.73",
             "unit": "ns"
           }
         ]
