b3eeae7832a982c9d030a571e4afb6504d5e4ac0
Kieker CI <ci-bot@kieker.org>
2026-05-09 06:42:06 +0000

    Benchmark Results 607e1be39104f58acdb760a1dd4d7bdea47a01f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778308681156,
+  "lastUpdate": 1778308926502,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -186823,6 +186823,52 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3462.47,
             "range": "3556.3",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "607e1be39104f58acdb760a1dd4d7bdea47a01f6",
+          "message": "Remove MOOBENCH_CONFIGURATIONS so all default configurations are used",
+          "timestamp": "2026-04-30T10:09:37Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/607e1be39104f58acdb760a1dd4d7bdea47a01f6"
+        },
+        "date": 1778308926476,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 37.1463,
+            "range": "37.1463",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 98.9484,
+            "range": "99.6205",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 2649.35,
+            "range": "2663.61",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 2850.83,
+            "range": "2856.57",
             "unit": "ns"
           }
         ]
