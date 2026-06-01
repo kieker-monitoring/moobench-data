ee21f9adea7592dd5048866a5a5fe31b5819a0a7
Kieker CI <ci-bot@kieker.org>
2026-06-01 10:19:50 +0000

    Benchmark Results f9b9d33003247a4250ddbb44c0b18fb21bacab17

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780307042309,
+  "lastUpdate": 1780309190766,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -226463,6 +226463,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 2026.53,
             "range": "2029.47",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "f9b9d33003247a4250ddbb44c0b18fb21bacab17",
+          "message": "Fixed Jenkins benchmarking commands",
+          "timestamp": "2026-04-29T10:23:03Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/f9b9d33003247a4250ddbb44c0b18fb21bacab17"
+        },
+        "date": 1780309190730,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.0025,
+            "range": "45.0028",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 832.739,
+            "range": "833.145",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 5034.68,
+            "range": "5037.49",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 5221.3,
+            "range": "5223.48",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 2017.32,
+            "range": "2018.07",
             "unit": "ns"
           }
         ]
