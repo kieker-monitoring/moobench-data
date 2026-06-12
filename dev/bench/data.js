df8b174afd4dfcaf675d2e74b07cfda4be74957d
Kieker CI <ci-bot@kieker.org>
2026-06-12 20:19:55 +0000

    Benchmark Results 53c9f2049ebe0b29c68841abbd544b201be707d4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781256980477,
+  "lastUpdate": 1781295595636,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -276567,6 +276567,72 @@ window.BENCHMARK_DATA = {
             "name": "Binary TCP - B",
             "value": 353.022,
             "range": "353.062",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "53c9f2049ebe0b29c68841abbd544b201be707d4",
+          "message": "Pinpoint does not work with Java 25 yet, so remove Java 25 for now",
+          "timestamp": "2026-06-12T21:36:59+02:00",
+          "tree_id": "409269d59be3154b597fdcf6a3fbd2a7070ae006",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/53c9f2049ebe0b29c68841abbd544b201be707d4"
+        },
+        "date": 1781295595597,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 367.341,
+            "range": "367.404",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe - A",
+            "value": 365.036,
+            "range": "365.051",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe - B",
+            "value": 365.939,
+            "range": "365.956",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging - A",
+            "value": 366.98,
+            "range": "367.075",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging - B",
+            "value": 368.104,
+            "range": "368.305",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary TCP - A",
+            "value": 367.2,
+            "range": "367.271",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary TCP - B",
+            "value": 365.655,
+            "range": "365.705",
             "unit": "ns"
           }
         ]
