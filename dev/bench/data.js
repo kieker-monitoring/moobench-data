1db7eb42465e5cd4b040541032a0a271e2a7bae8
Kieker CI <ci-bot@kieker.org>
2026-07-27 07:29:48 +0000

    Benchmark Results 34c1ab5b5a5ac454f0220c71047f2b05e5250751

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785137179043,
+  "lastUpdate": 1785137388304,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -139305,6 +139305,52 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 2184.63,
             "range": "2186.45",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shinhyung Yang",
+            "username": "shinhyungyang",
+            "email": "shinhyung.yang@email.uni-kiel.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "34c1ab5b5a5ac454f0220c71047f2b05e5250751",
+          "message": "Kieker-python fixes\n\n1. Added new hook routines for the post import hook.\n2. Minor updates has been made as kieker-lang-pack-python 0.0.2",
+          "timestamp": "2026-07-24T13:16:57Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/34c1ab5b5a5ac454f0220c71047f2b05e5250751"
+        },
+        "date": 1785137388256,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 53.0256,
+            "range": "53.0299",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 65.0428,
+            "range": "65.0489",
+            "unit": "ns"
+          },
+          {
+            "name": "No collection",
+            "value": 819.656,
+            "range": "819.901",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 2817.04,
+            "range": "2823.7",
             "unit": "ns"
           }
         ]
