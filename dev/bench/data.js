42a1ca98c967f11a454c863ebc4152eb35c55ec3
Kieker CI <ci-bot@kieker.org>
2026-08-09 05:13:30 +0000

    Benchmark Results d3111a1d865a7e614c028aecedd475ea7ebd3885

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786252020069,
+  "lastUpdate": 1786252410182,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -205957,6 +205957,52 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3557.86,
             "range": "3622.28",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "d3111a1d865a7e614c028aecedd475ea7ebd3885",
+          "message": "Add old filename to distinguish different configurations",
+          "timestamp": "2026-08-06T18:46:44Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/d3111a1d865a7e614c028aecedd475ea7ebd3885"
+        },
+        "date": 1786252410129,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 44.9203,
+            "range": "44.9204",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 142.345,
+            "range": "142.855",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3809.05,
+            "range": "3876.21",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3463.25,
+            "range": "3575.45",
             "unit": "ns"
           }
         ]
