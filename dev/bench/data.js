7d71c525d0cb515eee94411c02c1246be55a02e5
Kieker CI <ci-bot@kieker.org>
2026-08-23 05:29:55 +0000

    Benchmark Results 5fdc87a6055aec4284c50ea31b4f38168b374fb6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787460380976,
+  "lastUpdate": 1787462995484,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -251039,6 +251039,58 @@ window.BENCHMARK_DATA = {
             "name": "Prometheus",
             "value": 2015.33,
             "range": "2015.97",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "5fdc87a6055aec4284c50ea31b4f38168b374fb6",
+          "message": "Update to skywalking 9.7.0",
+          "timestamp": "2026-08-15T14:38:08Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/5fdc87a6055aec4284c50ea31b4f38168b374fb6"
+        },
+        "date": 1787462995443,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 37.2536,
+            "range": "37.2551",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 629.732,
+            "range": "630.794",
+            "unit": "ns"
+          },
+          {
+            "name": "No logging",
+            "value": 3583.56,
+            "range": "3584.18",
+            "unit": "ns"
+          },
+          {
+            "name": "Zipkin",
+            "value": 3756.86,
+            "range": "3759.8",
+            "unit": "ns"
+          },
+          {
+            "name": "Prometheus",
+            "value": 1538.67,
+            "range": "1539.08",
             "unit": "ns"
           }
         ]
