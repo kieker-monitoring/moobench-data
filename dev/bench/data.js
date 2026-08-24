574471fa753eeec38ea4329fedfc1268872c52a2
Kieker CI <ci-bot@kieker.org>
2026-08-24 09:54:37 +0000

    Benchmark Results 3ef83740eb8f4ee58832b3cde03f1a0b0be68136

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787564631721,
+  "lastUpdate": 1787565277304,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -208734,6 +208734,54 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3482.59,
             "range": "3589.39",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "DaGeRe",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3ef83740eb8f4ee58832b3cde03f1a0b0be68136",
+          "message": "Merge pull request #70 from Quomodo-0/implementation-k-perf-kotlin\n\nImplementation of k-perf Kotlin benchmark",
+          "timestamp": "2026-08-24T10:58:18+02:00",
+          "tree_id": "2a1931d5372be635ba0dd248e7dab172f70209f5",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/3ef83740eb8f4ee58832b3cde03f1a0b0be68136"
+        },
+        "date": 1787565277263,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 47.877,
+            "range": "47.877",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 144.528,
+            "range": "145.673",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3233.27,
+            "range": "3319.6",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3108.19,
+            "range": "3188.12",
             "unit": "ns"
           }
         ]
