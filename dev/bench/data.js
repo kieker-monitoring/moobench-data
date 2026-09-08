328b266897f343aac927ad141adf1eecf138f494
Kieker CI <ci-bot@kieker.org>
2026-09-08 08:47:36 +0000

    Benchmark Results 197240cd8f8686d7131664f1caba245502508082

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788857208141,
+  "lastUpdate": 1788857256671,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -211848,6 +211848,51 @@ window.BENCHMARK_DATA = {
             "name": "Writing without sanitizing fields",
             "value": 3327.27,
             "range": "3390.03",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jannik Heinzlreiter",
+            "email": "j.heinzlreiter@decom.at"
+          },
+          "committer": {
+            "name": "DaGeRe",
+            "username": "DaGeRe",
+            "email": "davidgeorg_reichelt@dagere.de"
+          },
+          "id": "197240cd8f8686d7131664f1caba245502508082",
+          "message": "Formatting script, adding parameters and removing \"--no-daemon\"",
+          "timestamp": "2026-09-04T12:26:29Z",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/197240cd8f8686d7131664f1caba245502508082"
+        },
+        "date": 1788857256619,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 45.226,
+            "range": "45.2347",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 143.243,
+            "range": "143.351",
+            "unit": "ns"
+          },
+          {
+            "name": "Regular Writing",
+            "value": 3165.46,
+            "range": "3246.11",
+            "unit": "ns"
+          },
+          {
+            "name": "Writing without sanitizing fields",
+            "value": 3222.03,
+            "range": "3341.52",
             "unit": "ns"
           }
         ]
