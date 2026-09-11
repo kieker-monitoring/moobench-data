887550aa8ac14ee27967a3dd095c94c7ed7a964d
Kieker CI <ci-bot@kieker.org>
2026-09-11 16:22:08 +0000

    Benchmark Results 16ddafc1605da2f3938f8de2948ed71852daf3de

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789142203796,
+  "lastUpdate": 1789143728381,
   "repoUrl": "https://github.com/kieker-monitoring/moobench",
   "entries": {
     "Kieker-java": [
@@ -57295,6 +57295,54 @@ window.BENCHMARK_DATA = {
             "name": "Binary file",
             "value": 983.371,
             "range": "984.768",
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "committer": {
+            "email": "davidgeorg_reichelt@dagere.de",
+            "name": "David Georg Reichelt",
+            "username": "DaGeRe"
+          },
+          "distinct": true,
+          "id": "16ddafc1605da2f3938f8de2948ed71852daf3de",
+          "message": "Update remaining workflows to ubuntu 26.04",
+          "timestamp": "2026-09-11T17:32:49+02:00",
+          "tree_id": "eb40c711929b308864c75b2747cf7ee56542f4ae",
+          "url": "https://github.com/kieker-monitoring/moobench/commit/16ddafc1605da2f3938f8de2948ed71852daf3de"
+        },
+        "date": 1789143728329,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "No instrumentation",
+            "value": 47.9725,
+            "range": "47.9735",
+            "unit": "ns"
+          },
+          {
+            "name": "Deactivated probe",
+            "value": 51.6795,
+            "range": "51.6812",
+            "unit": "ns"
+          },
+          {
+            "name": "No collection",
+            "value": 498.267,
+            "range": "498.284",
+            "unit": "ns"
+          },
+          {
+            "name": "Binary file",
+            "value": 1034.89,
+            "range": "1035.44",
             "unit": "ns"
           }
         ]
